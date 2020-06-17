e61a76135e5bf4f7ff1f89c712531b41c0d70517
Nick Routsong <nr@futuramicmedia.com>
2020-06-17 12:02:43 -0700

    Adding prove code, changing output to hello team/prove for root path

--- weeks/09/team/app.js
+++ weeks/09/team/app.js
@@ -8,7 +8,7 @@ app.use(express.static("public"));
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello Team!'));
 
 app.get('/equate', (req, res) => {
 
