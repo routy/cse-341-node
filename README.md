e03e48d132653025b2b4e95e8d59f78fae6cf2d7
Nick Routsong <nr@futuramicmedia.com>
2020-07-11 15:01:21 -0700

    Adding ability to mark a recipe as a favorite

--- README.md
+++ README.md
@@ -4,29 +4,29 @@
 
 |-CSE-341-NODE  
 |--weeks  
-|---11  
+|---12  
 |----prove  
 |----team  
 
 - Create the applications within Heroku with the week's prefix (must have a credit card attached to account in order to create more than 5 apps):
   
-  `heroku create -a w11-team -r w11-team`  
-  `heroku create -a w11-prove -r w11-prove`  
+  `heroku create -a w12-team -r w12-team`  
+  `heroku create -a w12-prove -r w12-prove`  
 
 - Attach the build packs to the applications
 
-  `heroku buildpacks:add -a w11-team https://github.com/timanovsky/subdir-heroku-buildpack`  
-  `heroku buildpacks:add -a w11-team heroku/nodejs`  
+  `heroku buildpacks:add -a w12-team https://github.com/timanovsky/subdir-heroku-buildpack`  
+  `heroku buildpacks:add -a w12-team heroku/nodejs`  
   
-  `heroku buildpacks:add -a w11-prove https://github.com/timanovsky/subdir-heroku-buildpack`  
-  `heroku buildpacks:add -a w11-prove heroku/nodejs`  
+  `heroku buildpacks:add -a w12-prove https://github.com/timanovsky/subdir-heroku-buildpack`  
+  `heroku buildpacks:add -a w12-prove heroku/nodejs`  
 
 - Set the configuration param to tell the build pack what the project's root path is  
   
-  `heroku config:set -a w11-team PROJECT_PATH=weeks/11/team`  
-  `heroku config:set -a w11-prove PROJECT_PATH=weeks/11/prove`  
+  `heroku config:set -a w12-team PROJECT_PATH=weeks/12/team`  
+  `heroku config:set -a w12-prove PROJECT_PATH=weeks/12/prove`  
   
 - Push to the repositories to build the apps  
   
-  `git push w11-team master`  
-  `git push w11-prove master`  
+  `git push w12-team master`  
+  `git push w12-prove master`  
--- weeks/12/team/app.js
+++ weeks/12/team/app.js
@@ -1,19 +1,69 @@
 const express  = require('express');
 const app      = express();
 const url      = require('url');
-const axios    = require('axios').default;
+var session    = require('express-session');
+const bcrypt   = require('bcrypt');
+const { Pool } = require("pg");
+var FileStore  = require('session-file-store')(session);
 
 require('dotenv').config();
 
-const port = process.env.PORT || '8001';
+/**
+ * Setup the session store
+ */
+app.use(session({
+  name: 'server-session-cookie-id',
+  secret: 'SDfimf3409masdc0923imasdfmp',
+  saveUninitialized: true,
+  resave: true,
+  store: new FileStore()
+}));
+
+/**
+ * Create a logger middleware
+ */
+
+var logRequest = function (req, res, next) {
+    console.log("Received a request for: " + req.url);
+    next();
+}
+app.use(logRequest);
+
+var verifyLogin = function (req, res, next) {
+    if(req.session && req.session.username) {
+        console.log("Session exists, continue.");
+        next();
+    } else {
+        res.status(401);
+        res.json({ success: false, message: 'unauthorized'});
+    }    
+}
+
+const port = process.env.PORT || '8005';
 app.set("port", port);
 
 app.use(express.static("public"));
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-// OMDB API URL
-const api_url = '';
+/**
+ * Parse the passed POST parameters
+ */
+app.use(express.urlencoded({
+    extended: true
+}));
+
+/**
+ * Setup the Pool
+ */
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({
+    connectionString: connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 /**
  * GET /
@@ -23,49 +73,53 @@ app.get('/', function(req, res) {
 });
 
 /**
- * GET /search?s={searchTerm}
+ * Login
  */
-app.get('/search', function(req, res) {
+app.post('/login', async function(req, res) {
 
-    const searchTerm = req.query.s;
+    const username = req.body.username;
+    const password = req.body.password;
 
-    if (!searchTerm) {
-        res.json({ status: 'error', message: 'You must provide a string to search for.'});
+    let status = false;
+
+    const users = [
+        { userId: 1, username: 'admin1', password: await bcrypt.hash( 'password1', 4 ) },
+        { userId: 2, username: 'admin2', password: await bcrypt.hash( 'password2', 4 ) },
+        { userId: 3, username: 'admin3', password: await bcrypt.hash( 'password3', 4 ) },
+        { userId: 4, username: 'admin4', password: await bcrypt.hash( 'password4', 4 ) } 
+    ];
+
+    let user = users.find(function( user ) {
+        return (user.username === username && bcrypt.compare(password, user.password));
+    });
+
+    if (user) {
+        req.session.username = username;
+        status = true;
     }
 
-    axios.get(api_url + '...').then(function (response) {
+    res.json({ success: status });
 
-        let data = [];
-
-        if (response.data) {
-            data = response.data
-        }
-
-        res.json({ status: 'success', data: data });
-    });
 });
 
 /**
- * GET /details?id={id}
+ * logout
  */
-app.get('/details', function(req, res) {
-
-    const id = req.query.id;
-
-    if (!id) {
-        res.json({ status: 'error', message: 'You must provide an ID to search on.'});
+app.post('/logout', function(req, res) {
+    if (req.session && req.session.username) {
+        delete req.session.username;
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
     }
+});
 
-    axios.get(api_url + '...').then(function (response) {
-
-        let data = [];
-
-        if (response.data) {
-            data = response.data
-        }
-
-        res.json({ status: 'success', data: data });
-    });
+/**
+ * getServerTime
+ */
+app.get('/getServerTime', verifyLogin, function(req, res) {
+    var time = new Date();
+    res.json({ success: true, time });
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
